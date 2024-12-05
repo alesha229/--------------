0149b27ecd15b0d77c6a3b5b403092c735378cab
alesha229 <dfyz123123123123@mail.ru>
2024-12-05 15:15:08 +0300

    initial commit

---
+++ task2.js
@@ -0,0 +1,100 @@
+let admin, name;
+name = "Джон"
+admin = name
+alert(admin)
+
+let planet,userName
+
+const BIRTHDAY = "18.04.1982"
+
+const ageConst = someCode(BIRTHDAY)
+
+let name1 =  `ilya`
+
+alert(`hello${1}`)
+alert(`hello${"name"}`)
+alert(`hello${name1} and ${name}`)
+
+let age = prompt("Как тебя зовут?", "неизвестно")
+
+
+alert(`Тебя зовут: ${age}`)
+
+
+let a = 1, b = 1;
+
+let c = ++a; // ? 2
+let d = b++; // ? 1
+
+
+//
+//
+//
+
+"" + 1 + 0 // 10 string 
+"" - 1 + 0 // -1
+true + false // 1
+6 / "3" // 2
+"2" * "3" // 6
+4 + 5 + "px" // 9px string
+"$" + 4 + 5 //$45
+"4" - 2 // 2
+"4px" - 2 // NaN
+"  -9  " + 5 // " -9  5"
+"  -9  " - 5 // -14
+null + 1 // 1
+undefined + 1 // Nan
+" \t \n" - 2 //-2
+
+//
+//10 из 14 правильно
+//
+
+
+
+//
+//
+//
+
+// let A = promt("Первое число",1)
+// let B = promt("Второе число",2)
+// alert(A+B)
+
+let A = promt("Первое число",1)
+let B = promt("Второе число",2)
+alert(+A + +B)
+
+//
+//Правильно
+//
+
+
+//
+//
+//
+
+5 > 4// true
+"ананас" > "яблоко"// false 
+"2" > "12"//true
+undefined == null//true
+undefined === null//false
+null == "\n0\n"//false
+null === +"\n0\n"//false
+
+//
+//2 неправильно
+//
+
+//
+//
+//
+
+if ("0") {
+    alert( 'Привет' );
+  }
+// выведет алерт
+
+
+//
+//
+//
